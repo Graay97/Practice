5e386bc803b4afd19b7febb22312d731789105c5
Graay97 <sag04043@naver.com>
2024-09-26 17:58:35 +0900

    second commit

--- cpp/1.cpp
+++ cpp/1.cpp
@@ -25,28 +25,41 @@ int main() {
 */
 
 // 1-2 정수형 자료형과 실수형 자료형
+/*
 #include <iostream>
 #include <climits>
 using namespace std;
 
 int main() {
 
-    int n_int = INT_MAX;
-    short n_short = SHRT_MAX;
-    long n_long = LONG_MAX;
-    long long n_llong = LLONG_MAX;
+    float a = 3.14;
+    int b = 3.14;
 
-    cout << "int는 " << sizeof n_int << "바이트이다." << endl;
-    cout << "이 바이트의 최대값은 " << n_int << " 이다." << endl;
-
-    cout << "short는 " << sizeof n_short << "바이트이다." << endl;
-    cout << "이 바이트의 최대값은 " << n_short << " 이다." << endl;
-
-    cout << "long은 " << sizeof n_long << "바이트이다." << endl;
-    cout << "이 바이트의 최대값은 " << n_long << " 이다." << endl;
-
-    cout << "long long은 " << sizeof n_llong << "바이트이다." << endl;
-    cout << "이 바이트의 최대값은 " << n_llong << " 이다." << endl;
+    cout << "a = " << a << " b = " << b << endl;
     
+    return 0;
+}
+*/
+
+// 1-3 문자형 자료형과 bool형 자료형
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a = 77;
+    char b = a;
+    cout << b << endl;
+
+    // 직접 문자를 입력하는 방식은 작은따옴표(' ') 사용.
+    // 1. 바로 집어넣기
+    char c = 'a';
+    cout << c << endl;
+    // 2. 변수를 선언하고 집어넣기
+    char d;
+    d = 'b';
+    cout << d << endl;
+
     return 0;
 }
