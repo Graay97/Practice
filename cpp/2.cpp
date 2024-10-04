45754212ba451bf020493b9ba094909cd0b69738
Graay97 <sag04043@naver.com>
2024-10-04 15:41:40 +0900

    8th

--- cpp/2.cpp
+++ cpp/2.cpp
@@ -43,6 +43,7 @@ int main()
 }
 */
 
+/*
 // 2-3 구조체
 #include <iostream>
 
@@ -61,7 +62,6 @@ int main()
         int weight;
     } B;
     
-    /*
     MyStruct A = {
         "Son",
         "striker",
@@ -79,7 +79,6 @@ int main()
     B = {};
 
     cout << B.height << endl;
-    */
 
     MyStruct A[2] = {
         {"A", "B", 1, 2},
@@ -87,6 +86,79 @@ int main()
     };
 
     cout << A[1].name << endl;
+
+    return 0;
+}
+*/
+
+/*
+// 2-4 공용체와 열거체
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    enum spectrum { red, orange, yello, green,
+    blue, violet, indigo, ultraviolet};
+
+    spectrum a = indigo;
+    cout << a << endl;
+
+    // 열거체는 기호상수로 관리되기 때문에 산술연산을 적용할 수 없다. 
+    spectrum b = red + orange; (X)
+    // 열거자들을 정수형에 대입할 때는 int형으로 변환 될 수 있다.
+    int c;
+    c = blue;
+    c = blue + 3;
+    cout << c << endl;
+
+    // 열거자들은 아무런 옵션을 지정하지 않으면 0부터 시작해서 1씩 더해지는 정수가 자동으로 저장되지만
+    // 명시적으로 대입연산자 이퀄(=)을 이용해서 값을 넣어줄 수 있다.
+    enum spectrum1 { red = 0, orange = 2, yello = 4, green,
+    blue, violet, indigo, ultraviolet};
+    // 열거자들을 초기화 할 때는 반드시 정수여야만 한다.
+    // 초기화 하지 않은 열거자들은 이전의 열거자의 값보다 1씩 더한 값을 가지게 된다.
+    enum spectrum1 { red = 0, orange = 2, yello = 4, green(5),
+    blue(6), violet(7), indigo(8), ultraviolet(9)};
+
+    return 0;
+}
+*/
+
+/*
+// 2-5 포인터와 메모리 해제(1)
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 6;
+    int* b;
+
+    b = &a;
+
+    cout << "a의 값 " << a << endl;
+    cout << "*b의 값 " << *b << endl;
+
+    cout << "a의 주소 " << &a << endl;
+    cout << "*b의 주소" << b << endl;
+
+    *b = *b + 1;
+    cout << "이제 a의 값은 " << a << endl;  
+
+    return 0;
+}
+*/
+
+// 2-6 포인터와 메모리 해제(2)
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
 
     return 0;
 }
